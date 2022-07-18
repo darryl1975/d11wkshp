40c922e0d32add9d387c295d84b7876305b73929
darryl1975 <darryl1975@hotmail.com>
2022-07-18 13:53:03 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/d11wkshp/D11wkshpApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d11wkshp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D11wkshpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D11wkshpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/d11wkshp/D11wkshpApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d11wkshp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D11wkshpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
