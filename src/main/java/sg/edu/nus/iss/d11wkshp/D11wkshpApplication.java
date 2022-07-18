d7efd44ffb1e4128e8ddab07479fe3d651ab62c0
darryl1975 <darryl1975@hotmail.com>
2022-07-18 16:06:11 +0800

    added applicationarguments

--- src/main/java/sg/edu/nus/iss/d11wkshp/D11wkshpApplication.java
+++ src/main/java/sg/edu/nus/iss/d11wkshp/D11wkshpApplication.java
@@ -3,11 +3,27 @@ package sg.edu.nus.iss.d11wkshp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
+import java.util.Collections;
+
+
 @SpringBootApplication
 public class D11wkshpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(D11wkshpApplication.class, args);
+		//SpringApplication.run(D11wkshpApplication.class, args);
+		SpringApplication app = new SpringApplication(D11wkshpApplication.class);
+
+		String port = "8080";
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+
+		if (cliOpts.containsOption("port")) {
+			port = cliOpts.getOptionValues("port").get(0);
+		}
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
 	}
 
 }
